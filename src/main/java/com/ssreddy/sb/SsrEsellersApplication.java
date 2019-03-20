f989ba006789a1156dab0b4029d95cedf29f585a
Shyam <tshyam.tr@gmail.com>
2019-03-20 19:48:39 +0530

    Initial commit.

---
+++ src/main/java/com/ssreddy/sb/SsrEsellersApplication.java
@@ -0,0 +1,13 @@
+package com.ssreddy.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsrEsellersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsrEsellersApplication.class, args);
+    }
+
+}
